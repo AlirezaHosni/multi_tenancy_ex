08bbbf2a7675417253843ac49b30d43fb344c239
Alireza Hosni <alireza79hosni@gmail.com>
2022-07-17 02:24:25 +0430

    change domain to route parameter

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\RegisteredTenantController;
 use Illuminate\Support\Facades\Route;
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +22,54 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::middleware(InitializeTenancyByDomain::class)->prefix('{tenant}')->group(function (){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/dashboard', function () {
+        return \App\Models\User::all();
+    })->middleware(['auth']);
 
-Route::get('register', [RegisteredTenantController::class, 'create']);
-Route::post('register', [RegisteredTenantController::class, 'store'])->name('register.tenant');
+    Route::middleware('guest')->group(function () {
+
+        Route::get('login', [AuthenticatedSessionController::class, 'create'])
+            ;
+
+        Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+        Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
+            ->name('password.request');
+
+        Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
+            ->name('password.email');
+
+        Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
+            ->name('password.reset');
+
+        Route::post('reset-password', [NewPasswordController::class, 'store'])
+            ->name('password.update');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::get('verify-email', [EmailVerificationPromptController::class, '__invoke'])
+            ->name('verification.notice');
+
+        Route::get('verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+            ->middleware(['signed', 'throttle:6,1'])
+            ->name('verification.verify');
+
+        Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+            ->middleware('throttle:6,1')
+            ->name('verification.send');
+
+        Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
+            ->name('password.confirm');
+
+        Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
+
+        Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
+            ->name('logout');
+    });
+    Route::get('register', [RegisteredTenantController::class, 'create']);
+    Route::post('register', [RegisteredTenantController::class, 'store'])->name('register.tenant');
+});
